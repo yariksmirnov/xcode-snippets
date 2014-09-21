a78f4fc2859a53bbe9f10b7189438774b1050754
Yarik Smirnov <yaroslav.smirnov@softfacade.com>
2014-09-21 21:50:53 +0400

    Imported user code snippets

---
+++ pragma_mark.m
@@ -0,0 +1,10 @@
+// Pragma mark
+// Add #pragma mark 
+//
+// IDECodeSnippetCompletionPrefix: pragma
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: E3EF06F0-553D-4F15-880A-C715B3172507
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+#pragma mark - <#Name#>
---
+++ selector_check_.m
@@ -0,0 +1,12 @@
+// Selector Check 
+// Check if self.delegate responds to some selector
+//
+// IDECodeSnippetCompletionPrefix: responds
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: E1D4A088-398C-41D9-9DD3-F4E82665236D
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+if ([self.delegate respondsToSelector:@selector(<#selector#>)]) {
+    [self.delegate <#selector#>];
+    }
